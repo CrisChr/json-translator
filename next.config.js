662ce10a85c5bbc4e530ca73390118a0f6a9110d
CrisChr <chr7749@hotmail.com>
2025-03-11 17:59:25 +0800

    update: add cache handler for memory usage

--- next.config.js
+++ next.config.js
@@ -2,8 +2,37 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    scrollRestoration: true
-  }
+    scrollRestoration: true,
+    serverSourceMaps: false,
+    webpackBuildWorker: true
+  },
+  cacheHandler: require.resolve('./cache-handler.js'),
+  cacheMaxMemorySize: 0, // disable default in-memory caching
+  productionBrowserSourceMaps: false,
+  webpack: (
+    config,
+    { dev }
+  ) => {
+    if (config.cache && !dev) {
+      config.cache = Object.freeze({
+        type: 'memory',
+      })
+    }
+    // Important: return the modified config
+    return config
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
 
 module.exports = nextConfig
